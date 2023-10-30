0a7ec6a5426c3fa7ad07d3da5f047e2a294308ae
Nastasica <nastena.terekhova.92@mail.ru>
2023-10-30 12:35:35 +0300

    создает модуль big-pictures.js

--- js/mini-pictures.js
+++ js/mini-pictures.js
@@ -1,4 +1,4 @@
-
+import {showBigPicture} from './big-pictures.js';
 function renderPhotos(photos) {
   const picturesContainer = document.querySelector('.pictures');
   const fragment = document.createDocumentFragment();
@@ -6,14 +6,18 @@ function renderPhotos(photos) {
   picturesBlockElement.querySelector('.pictures__title').classList.remove('visually-hidden');
   photos.forEach((photo) => {
     const pictureTemplate = document.querySelector('#picture').content.cloneNode(true);
-    const picture = pictureTemplate.querySelector('.picture');
+    const thumbnail = pictureTemplate.querySelector('.picture');
 
-    picture.querySelector('.picture__img').src = photo.url;
-    picture.querySelector('.picture__img').alt = photo.description;
-    picture.querySelector('.picture__likes').textContent = photo.likes;
-    picture.querySelector('.picture__comments').textContent = photo.comments.length;
+    thumbnail.querySelector('.picture__img').src = photo.url;
+    thumbnail.querySelector('.picture__img').alt = photo.description;
+    thumbnail.querySelector('.picture__likes').textContent = photo.likes;
+    thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
 
-    fragment.appendChild(picture);
+    thumbnail.addEventListener('click', () => {
+      showBigPicture(photo); // Вызываем функцию полноразмерного режима и передаем данные фотографии
+    });
+
+    fragment.appendChild(thumbnail);
   });
 
   picturesContainer.appendChild(fragment);
