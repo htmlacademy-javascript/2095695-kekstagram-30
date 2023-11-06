29673769610f112d9dac95ef36bc4dea863538d4
Nastasica <nastena.terekhova.92@mail.ru>
2023-11-06 22:53:04 +0300

    создает модуль img-upload-form.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ import { getPictures } from './get-pictures.js';
 import { renderGallery } from './gallery.js';
 const AMOUNT_OF_DESCRIPTION = 25;
 renderGallery(getPictures(AMOUNT_OF_DESCRIPTION));
+import './img-upload-form.js';
+import './form-validator.js';
+
