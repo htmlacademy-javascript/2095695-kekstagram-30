7d08f41c8be815eea3c9d8fa094163412ca65d9b
Nastasica <nastena.terekhova.92@mail.ru>
2023-11-06 13:11:26 +0300

    создает модуль form-validator.js

---
+++ js/form-validator.js
@@ -0,0 +1,35 @@
+// const form = document.querySelector('.img-upload__form');
+// const overlay = document.querySelector('.img-upload__overlay');
+// const body = document.querySelector('body');
+// const cancelButton = document.querySelector('#upload-cancel');
+// const fileField = document.querySelector('#upload-file');
+// const hashtagField = document.querySelector('.text__hashtags');
+// const commentField = document.querySelector('.text__description');
+
+// const MAX_HASHTAG_COUNT = 5;
+// const MIN_HASHTAG_LENGTH = 2;
+// const MAX_HASHTAG_LENGTH = 20;
+// const UNVALID_SYMBOLS = /[^a-zA-Z0-9а-яА-ЯёЁ]/g;
+
+
+// const showModal = () => {
+//   overlay.classList.remove('hidden');
+//   body.classList.add('modal-open');
+//   document.addEventListener('keydown', onEscKeyDown);
+// };
+
+// const hideModal = () => {
+//   form.reset();
+//   pristine.reset();
+//   overlay.classList.add('hidden');
+//   body.classList.remove('modal-open');
+//   document.removeEventListener('keydown', onEscKeyDown);
+// };
+
+// function onEscKeyDown(evt) {
+//   if (evt.key === 'Escape' && !isTextFieldFocused()) {
+//     evt.preventDefault();
+//     hideModal();
+//   }
+// }
+
